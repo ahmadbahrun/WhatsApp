f4393d6799d97618656f16a00b3b3bbd92950583
ahmadbahrun <redgaming18@gmail.com>
2023-02-21 21:32:42 +0700

    add SplashScreen.kt

---
+++ app/src/main/java/com/example/whatsapp/splashscreen/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.example.whatsapp.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.whatsapp.MainActivity
+import com.example.whatsapp.R
+import com.example.whatsapp.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreen,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
